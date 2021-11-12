8be82520477045c1a0da168e87a7923ce537a24f
Johnny Wu <johnnyw131@gmail.com>
2021-11-12 01:43:15 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+function writeCards(Name, surprise){
+    let message = [];
+    for (let i=0; i <Name.length; i++){
+        message.push(`Thank you, ${Name[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return message;
+};
+
+writeCards(message);
+
+
+function countDown(i){
+    while (i+1 > 0){
+        console.log(i);
+        i--;
+    }
+    
+}
